3a49d5b6639e834f9960f4c90890165b8c4a5fea
MeiyinPU <mei-yin.pu@oclock.school>
2023-11-01 15:34:36 +0100

    init environment

---
+++ index.js
@@ -0,0 +1,55 @@
+// varialbes d'env
+require('dotenv').config();
+
+// package express
+const express = require("express");
+
+
+// reuquire le module express-session
+
+// require router
+// const router = require(".app/router.js");
+
+// execution d'express
+const app = express();
+
+// config views
+app.set('view engine', 'ejs');
+app.set('views', './app/views');
+
+// config fichier statiques
+app.use(express.static("./public"));
+
+//accès à request.body dans les controllers
+
+
+// branche le MW express-session 
+
+// MW custom àpres "session" et avant "router"
+// app.use((req, res, next)=>{
+// res.locals.userName = req.session.
+// next();
+// })
+
+// router
+// app.use(router);
+
+
+// test
+app.get("/", (req, res)=>{
+  res.write("hello world");
+  res.end();
+}
+);
+
+app.use((req, res) => {
+  res.status(404).render('404');
+});
+
+// port à écouter
+const PORT = process.env.PORT || 8080;
+// const HOST = process.env.HOST;
+
+app.listen(PORT, ()=>{
+  console.log(`App running at http://lcoalhost:${PORT}`);
+});
